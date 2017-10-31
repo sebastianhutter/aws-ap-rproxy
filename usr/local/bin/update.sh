698f41162e7549fada991169ccd4282f47a9d873
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-10-31 15:00:09 +0100

    overwrite content and not file

--- usr/local/bin/update.sh
+++ usr/local/bin/update.sh
@@ -2,6 +2,8 @@
 {%- set s3bucket = "ars-pugnandi-haproxy-configuration-eu-central-1" %}
                   #!/bin/bash
                   echo "$(date) : copy new haproxy.cfg"
-                  aws --region eu-central-1 s3 cp s3://{{s3bucket}}/haproxy.cfg /opt/haproxy.cfg
+                  aws --region eu-central-1 s3 cp s3://{{s3bucket}}/haproxy.cfg /tmp/haproxy.cfg
+                  cat /tmp/haproxy.cfg > /opt/haproxy.cfg
+                  rm /tmp/haproxy.cfg
                   echo "$(date) : reload haproxy"
                   sudo docker kill -s HUP haproxy
